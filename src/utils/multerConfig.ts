733fe84f83f972ebad03d1e64d11a3bf4dcc29a8
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-10 14:59:48 -0600

    Test #% for heroku deploy

--- src/utils/multerConfig.ts
+++ src/utils/multerConfig.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export const diskStorageObjest: DiskStorageOptions = {
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "dist/image-uploader-api/img"));
+    cb(null, path.join(__dirname, "../dist//img"));
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
