628e0837f78b1d8bd84751f886df87248c859839
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-10 16:03:11 -0600

    Test

--- src/utils/multerConfig.ts
+++ src/utils/multerConfig.ts
@@ -2,7 +2,7 @@ import { DiskStorageOptions } from "multer";
 
 export const diskStorageObjest: DiskStorageOptions = {
   destination: (req, file, cb) => {
-    cb(null, "../img");
+    cb(null, "../../img");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
