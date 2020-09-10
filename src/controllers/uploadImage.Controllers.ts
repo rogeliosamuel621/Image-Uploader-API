7188ba506559db5eb3609be7ea07423f6560385d
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-10 17:27:12 -0600

    I think it finales work

--- src/controllers/uploadImage.Controllers.ts
+++ src/controllers/uploadImage.Controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { v2 } from "cloudinary";
 import { INTERNAL_SERVER_ERROR, OK, BAD_REQUEST } from "http-status-codes";
+import path from "path";
 
 export function uploadImageController(req: Request, res: Response): any {
   if (!req.file) {
@@ -12,12 +13,12 @@ export function uploadImageController(req: Request, res: Response): any {
     });
   }
 
-  const url = `app/dist/img/${req.file.originalname}`;
+  const url = __dirname + `../../../img/${req.file.originalname}`;
 
   v2.uploader.upload(url, (err, result) => {
     if (err) {
       return res.json({
-        error: true,
+        error: err,
         statusCode: INTERNAL_SERVER_ERROR,
         data: null,
         message: "An error has occurred uploading your image, try later",
