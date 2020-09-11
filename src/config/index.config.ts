8411f1e4a1ca7f2813135c661e40e43bb1f07063
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-11 08:38:07 -0600

    [UPDATE] API_KEY

--- src/config/index.config.ts
+++ src/config/index.config.ts
@@ -1,8 +1,8 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
 
 export const port = process.env.PORT as string;
-export const api_key = process.env.API_KEY;
+export const api_key = process.env.API_KEY as string;
 export const jwt_key = process.env.JWT_SECRET;
 export const cloudinary_cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
 export const cloudinary_api_key = process.env.CLOUDINARY_API_KEY;
